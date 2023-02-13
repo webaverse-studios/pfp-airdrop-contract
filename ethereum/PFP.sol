90039584e5e2b448351f7d19b01896f0199e4bcf
blockchainFull2022 <montealegrefamiliar@gmail.com>
2023-02-12 19:06:53 -0800

    add airdrop function

--- ethereum/PFP.sol
+++ ethereum/PFP.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "./ERC721A.sol";
 
 contract PFP is ERC721A, Ownable {
@@ -14,7 +16,9 @@ contract PFP is ERC721A, Ownable {
     bool public saleIsActive = false;
     string public baseExtension = ".json";
     string private _baseURIextended;
-    address private _passAddress;
+    address public _passAddress;
+
+    mapping (address => bool) private _isAirdropped;
 
     // WhiteLists for presale.
     // mapping (address => bool) private _isWhiteListed;
@@ -33,6 +37,14 @@ contract PFP is ERC721A, Ownable {
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseURIextended;
     }
+    
+    function setPassContract(address passAddress_) public onlyOwner {
+        _passAddress = passAddress_;
+    }
+
+    function getPassContract() public view returns (address) {
+        return _passAddress;
+    }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory)
     {
@@ -49,10 +61,23 @@ contract PFP is ERC721A, Ownable {
     }
 
 
-    function mintToken(uint numberOfTokens) public payable {
+    function mintToken(uint numberOfTokens) public {
         require(saleIsActive, "Sale must be active to mint Tokens");
         require(totalSupply() + numberOfTokens <= MAX_TOKENS, "Purchase would exceed max supply of tokens");
         _safeMint(msg.sender, numberOfTokens);
+    }
+
+    function airdropToken() public onlyOwner {
+        uint256 passSupply = uint256(IERC721Enumerable(_passAddress).totalSupply());
+        
+        for(uint i=0; i<passSupply; i++) {
+            address passOwner = IERC721(_passAddress).ownerOf(i);
+            if(!_isAirdropped[passOwner]) {
+                uint256 passBalance = IERC721(_passAddress).balanceOf(passOwner);
+                _safeMint(passOwner, passBalance);
+                _isAirdropped[passOwner] = true;
+            }
+        }        
     }
 
     function withdraw() public onlyOwner {
